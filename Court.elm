bc00f410302d78773f744682cd07728ca1f5366a
James Hopkins <jameshopkins0709@gmail.com>
2017-07-02 12:30:58 +0100

    decouple pitch entity from main application

---
+++ Court.elm
@@ -0,0 +1,66 @@
+module Court exposing (..)
+
+import WebGL exposing (Mesh, Shader, Entity)
+import Math.Vector2 as Vec2 exposing (Vec2, vec2)
+import Math.Vector3 as Vec3 exposing (vec3, Vec3)
+import Math.Matrix4 as Mat4 exposing (Mat4)
+import WebGL.Texture as Texture exposing (Error, Texture)
+
+
+type alias Uniforms =
+    { perspective : Mat4
+    , texture : Texture
+    }
+
+
+type alias Vertex =
+    { position : Vec3
+    , coord : Vec2
+    }
+
+
+court : Mesh Vertex
+court =
+    let
+        nearLeft =
+            Vertex (vec3 -1 -1 -1) (vec2 0 1)
+
+        nearRight =
+            Vertex (vec3 1 -1 -1) (vec2 1 1)
+
+        farLeft =
+            Vertex (vec3 -1 -1 1) (vec2 0 0)
+
+        farRight =
+            Vertex (vec3 1 -1 1) (vec2 1 0)
+    in
+        [ ( nearLeft, nearRight, farLeft ), ( farLeft, farRight, nearRight ) ]
+            |> WebGL.triangles
+
+
+vertexShader : Shader Vertex Uniforms { vcoord : Vec2 }
+vertexShader =
+    [glsl|
+        attribute vec3 position;
+        attribute vec2 coord;
+        uniform mat4 perspective;
+        varying vec2 vcoord;
+
+        void main () {
+            gl_Position = perspective * vec4(position, 1.0);
+            vcoord = coord;
+        }
+    |]
+
+
+fragmentShader : Shader {} { u | texture : Texture } { vcoord : Vec2 }
+fragmentShader =
+    [glsl|
+        precision mediump float;
+        uniform sampler2D texture;
+        varying vec2 vcoord;
+
+        void main () {
+            gl_FragColor = texture2D(texture, vcoord);
+        }
+    |]
---
+++ Main.elm
@@ -0,0 +1,92 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Events exposing (onInput)
+import Html.Attributes exposing (width, height, min, max, style, type_, value)
+import Math.Matrix4 as Mat4 exposing (Mat4)
+import Math.Vector3 as Vec3 exposing (vec3, Vec3)
+import Task
+import WebGL exposing (Mesh, Shader, Entity)
+import WebGL.Texture as Texture exposing (Error, Texture)
+import Court
+
+
+type alias Model =
+    { texture : Maybe Texture, rotation : Float }
+
+
+type Msg
+    = TextureLoaded (Result Error Texture)
+    | RotateFloor String
+
+
+scene : Mat4 -> Texture -> List Entity
+scene camera texture =
+    [ WebGL.entity
+        Court.vertexShader
+        Court.fragmentShader
+        Court.court
+        { texture = texture, perspective = camera }
+    ]
+
+
+perspective : Float -> Mat4
+perspective angle =
+    List.foldr Mat4.mul
+        Mat4.identity
+        [ Mat4.makePerspective 45 1 0.01 100
+        , Mat4.makeLookAt (vec3 0 0 4) (vec3 0 0 0) (vec3 0 1 0)
+        , Mat4.makeRotate (angle / 230) (vec3 0 1 0)
+        ]
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , view = view
+        , subscriptions = (\_ -> Sub.none)
+        , update = update
+        }
+
+
+init : ( Model, Cmd Msg )
+init =
+    let
+        loadTexture =
+            Texture.load "crate.jpg"
+    in
+        ( Model Nothing 0, Task.attempt TextureLoaded loadTexture )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        RotateFloor deg ->
+            ( { model | rotation = Result.withDefault 0 (String.toFloat deg) }, Cmd.none )
+
+        TextureLoaded texture ->
+            ( { model | texture = Result.toMaybe texture }, Cmd.none )
+
+
+view : Model -> Html Msg
+view { rotation, texture } =
+    div []
+        [ WebGL.toHtml
+            [ width 800
+            , height 800
+            , style [ ( "display", "block" ) ]
+            ]
+            (texture
+                |> Maybe.map (scene (perspective rotation))
+                |> Maybe.withDefault []
+            )
+        , input
+            [ Html.Attributes.min "0"
+            , Html.Attributes.max "360"
+            , onInput RotateFloor
+            , type_ "range"
+            , value <| toString <| rotation
+            ]
+            []
+        ]
